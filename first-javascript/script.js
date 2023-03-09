aaf146f03cbd59951961ac7835771c7e06e019c8
mbliefer <mblief@gmail.com>
2023-03-08 20:24:15 -0600

    javascript

---
+++ first-javascript/script.js
@@ -0,0 +1,87 @@
+// console.log('Hello again');
+
+// var book ="Dracula";
+// var movie;
+// console.log(book);
+// console.log(movie);
+// movie = "Interview with a Vampire";
+// console.log(movie);
+
+// var count = "3";
+// var num = "2";
+// console.log(count + num);
+// console.log(5 + 8);
+// console.log("5" + "8");
+// console.log("5" + 8);
+// console.log(5 + "8");
+
+// var firstName = "Dan";
+// var lastName = "Taylor";
+
+// console.log(firstName + " " + lastName);
+
+// var cars = ["Subaru", "Audi", "Volkswagon"];
+// console.log("The third car in the list is " + cars[2]);
+// cars.push("Chevy");
+// cars.push("Ford");
+// cars.push("Honda");
+// cars.push("Toyota");
+// cars.push("Fiat");
+// cars.push("Buick");
+// cars.push("Jeep");
+// console.log(cars.length);
+// //["Subaru", "Audi", "Volkswagon", "Chevy", "Ford", "Honda", "Toyota", "Fiat", "Buick", "Jeep"]
+// console.log(cars);
+// console.log(cars[cars.length - 1]);
+// cars.pop();
+// console.log(cars.length);
+// console.log(cars[cars.length - 1]);
+// console.log(cars);
+// cars.splice(2, 3);
+// console.log(cars);
+// console.log(cars.length);
+// var student = {
+//     firstName: "Henry",
+//     lastName: "Jackson",
+//     id: 12345,
+//     courses: [
+//         {
+//             class: "Full-Stack Jave",
+//             instructor: {
+//                 firstName: "Steve",
+//                 lastName: "Wozniak"
+//             },
+//             gradePercentage: 97
+//         },
+//         {
+//             class: "Calculus",
+//             instructor: {
+//                 firstName: "Bill",
+//                 lastName: "Gates"
+//             },
+//             gradePercentage: 86
+//         }
+//     ]
+// }
+
+// console.log("My name is " + student.firstName + ' ' + student.lastName + ', and I am taking ' + student.courses.length + ' courses.');
+// console.log("My first course is " + student.courses[0].class + ', taught by ' + student.courses[0].instructor.firstName + ' ' + student.courses[0].instructor.lastName + '.'); 
+// console.log("I am getting a " + student.courses[0].gradePercentage + ' in ' + student.courses[0].class + '.');
+
+var zombies = 8;
+if (zombies > 10) {
+    console.log('AAAAAAAAAAAAAAARRRRGGG! ZOMBIES!!');
+} else if (zombies > 5) {
+    console.log("Gotta stay away from those zombies.");
+} else {
+    console.log('Not a lot of zombies today');
+}
+
+var apples = 15;
+if (apples > 3) {
+    console.log("Let's make tarts!")
+} else if (apples > 10) {
+    console.log('Let us make a pie! We can use the "Betty Crocker" cookbook!')
+}
+
+
