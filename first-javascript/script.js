b79b65f07a6988fe773c93b9f0527a9aebab173f
mbliefer <mblief@gmail.com>
2023-03-13 17:40:05 -0500

    practice

--- first-javascript/script.js
+++ first-javascript/script.js
@@ -68,20 +68,64 @@
 // console.log("My first course is " + student.courses[0].class + ', taught by ' + student.courses[0].instructor.firstName + ' ' + student.courses[0].instructor.lastName + '.'); 
 // console.log("I am getting a " + student.courses[0].gradePercentage + ' in ' + student.courses[0].class + '.');
 
-var zombies = 8;
-if (zombies > 10) {
-    console.log('AAAAAAAAAAAAAAARRRRGGG! ZOMBIES!!');
-} else if (zombies > 5) {
-    console.log("Gotta stay away from those zombies.");
-} else {
-    console.log('Not a lot of zombies today');
+// var zombies = 8;
+// if (zombies > 10) {
+//     console.log('AAAAAAAAAAAAAAARRRRGGG! ZOMBIES!!');
+// } else if (zombies > 5) {
+//     console.log("Gotta stay away from those zombies.");
+// } else {
+//     console.log('Not a lot of zombies today');
+// }
+
+// var apples = 15;
+// if (apples > 3) {
+//     console.log("Let's make tarts!")
+// } else if (apples > 10) {
+//     console.log('Let us make a pie! We can use the "Betty Crocker" cookbook!')
+// }
+
+// /////////////////////////////////////////////
+// Javascript assignment
+// 1.
+var userName;
+userName = 'Mark';
+var admin;
+admin = 'Bob';
+var firstNumber;
+firstNumber = 14;
+// 2.
+var driver = 'Jerry'
+var pitBoss = 'Hank'
+var carNumber = 23;
+//3.
+var company = {
+    name: 'ABC Company',
+    address: {
+        streetAddress: '123 South St.',
+        city: 'Omaha',
+        state: 'NE',
+        zip: '68137'
+    },
+    phoneNumber: '555-123-4567',
+    employeeCount: 25
 }
+// 4.
+console.log("The name of the company is " + company.name + ". It is located at " + 
+company.address.streetAddress + " " + company.address.city + ", " + company.address.state + 
+" " + company.address.zip + " and its phone number is " + company.phoneNumber + ". The company has " +
+company.employeeCount + " employess.");
 
-var apples = 15;
-if (apples > 3) {
-    console.log("Let's make tarts!")
-} else if (apples > 10) {
-    console.log('Let us make a pie! We can use the "Betty Crocker" cookbook!')
+// 5.
+var food = ["Bread", "Ham", "Cheese", "Pickles", "Mustard"];
+food.pop();
+food.push("Onions", "Chicken", "Turkey");
+food.splice(1, 4);
+console.log(food);
+// 6.
+var score = 70;
+if (score === 100) {
+    console.log("You aced it!");
 }
-
-
+else if (score > 69) {
+    console.log("You passed!");
+} else console.log("I'm sorry, you didn't pass.");
