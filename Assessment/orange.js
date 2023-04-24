81d358163b95abc781af1ef9d2ac84cd19132b8c
mbliefer <mblief@gmail.com>
2023-04-23 20:38:38 -0500

    Assessment done

--- Assessment/orange.js
+++ Assessment/orange.js
@@ -1,7 +1,12 @@
 $(function () {
    
-    $("article > img").click(function() {
+    $("nav > a").click(function() {
+        $("article").removeClass("show");
+        let target = $(this).attr("href");
+        $(target).addClass("show");     
+    });
     
-        
+    $("article > img").click(function() {
+        $("article").removeClass("show");
     });
 });
